caac0de47ecf80bee8c9ebe1110a5019710f7f61
RizkiAK <73286464+RizkiAK@users.noreply.github.com>
2021-09-21 20:16:58 +0700

    login endpoint

--- handler/user.go
+++ handler/user.go
@@ -46,3 +46,39 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	//user memasukkan input (email & password)
+	//input ditangkap handler
+	//mapping dari input user ke input struct
+	//input struct parsing ke service
+	//service mencari dengan bantuan repository user dengan emailx
+	//mencocokan password
+
+	var input user.LoginInput
+
+	err := c.ShouldBind(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.ApiResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+		response := helper.ApiResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokentoken")
+
+	response := helper.ApiResponse("Succesfully loggedin", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
